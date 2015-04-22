079e3eebc2ab520fd66d6e61cfd343e61b6409dd
Margaret Scott <margaretscott@patientslikeme.com>
2015-04-22 15:15:25 -0400

    add conjuring business logic

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -1,55 +1,66 @@
 require_relative 'item'
 
 class GildedRose
+  attr_reader :items
+
   MAX_QUALITY = 50
   MIN_QUALITY = 0
-  @items = []
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    items << Item.new("+5 Dexterity Vest", 10, 20)
+    items << Item.new("Aged Brie", 2, 0)
+    items << Item.new("Elixir of the Mongoose", 5, 7)
+    items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
+    items.each { |item| update_item(item) }
+  end
 
-    @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          decrease_item_quality(item)
-        end
-      else
-        if item.quality < MAX_QUALITY
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              increase_item_quality(item)
-            end
-            if item.sell_in < 6
-              increase_item_quality(item)
-            end
+  def update_item(item)
+    if item.name =~ /Conjured/
+      2.times { decrease_item_quality(item) }
+      item.sell_in -= 1
+      if (item.sell_in < 0)
+        2.times { decrease_item_quality(item) }
+      end
+      return
+    end
+
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        decrease_item_quality(item)
+      end
+    else
+      if item.quality < MAX_QUALITY
+        item.quality += 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.sell_in < 11
+            increase_item_quality(item)
+          end
+          if item.sell_in < 6
+            increase_item_quality(item)
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              decrease_item_quality(item)
-            end
-          else
-            item.quality = 0
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            decrease_item_quality(item)
           end
         else
-          increase_item_quality(item)
+          item.quality = 0
         end
+      else
+        increase_item_quality(item)
       end
     end
   end
